c1db8a66cf65007ed791d6ec774f98f5df01ba22
informant <artemiy.solodkin@mail.ru>
2023-04-01 22:26:37 +0500

    NEw Window

---
+++ CollisionEditor2/Views/OpenTileMap.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CollisionEditor2.Views
+{
+    public partial class OpenTileMap : Window
+    {
+        public OpenTileMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
