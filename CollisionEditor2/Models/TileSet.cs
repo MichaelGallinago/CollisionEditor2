ef017c7328f48580ca06d25b56950d578c4d56f3
Michael Gallinago <michael.gallinago@gmail.com>
2023-04-22 16:34:16 +0500

    Bruh

--- CollisionEditor2/Models/TileSet.cs
+++ CollisionEditor2/Models/TileSet.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Runtime.InteropServices;
+using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.Drawing;
 using System.IO;
 using System;
+using Microsoft.CodeAnalysis.Text;
 
 namespace CollisionEditor2.Models;
 
@@ -36,22 +38,22 @@ public class TileSet
 
         IntPtr ptr = bitmapData.Scan0;
         int bytes = Math.Abs(bitmapData.Stride) * TileSize.Height;
-        var ColorValues = new byte[bytes];
+        var colorValues = new byte[bytes];
 
-        System.Runtime.InteropServices.Marshal.Copy(ptr, ColorValues, 0, bytes);
-        for (int i = 0; i < ColorValues.Length; i++)
+        Marshal.Copy(ptr, colorValues, 0, bytes);
+        for (int i = 0; i < colorValues.Length; i++)
         {
             if (i % 4 == 3)
             {
-                if (ColorValues[i] > 0)
+                if (colorValues[i] > 0)
                 {
-                    ColorValues[i] = 255;
+                    colorValues[i] = 255;
                 }
                 continue;
             }
-            ColorValues[i] = 0;
+            colorValues[i] = 0;
         }
-        System.Runtime.InteropServices.Marshal.Copy(ColorValues, 0, ptr, bytes);
+        Marshal.Copy(colorValues, 0, ptr, bytes);
 
         tileMap.UnlockBits(bitmapData);
 
@@ -92,17 +94,29 @@ public class TileSet
             WidthMap.Add(new byte[TileSize.Height]);
             HeightMap.Add(new byte[TileSize.Width]);
 
+            BitmapData bitmapData = Tiles[i].LockBits(
+                new Rectangle(0, 0, TileSize.Width, TileSize.Height),
+                ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+            IntPtr ptr = bitmapData.Scan0;
+            int bytes = Math.Abs(bitmapData.Stride) * TileSize.Height;
+            var colorValues = new byte[bytes];
+
+            Marshal.Copy(ptr, colorValues, 0, bytes);
             for (int x = 0; x < TileSize.Width; x++)
             {
                 for (int y = 0; y < TileSize.Height; y++)
                 {
-                    if (Tiles[i].GetPixel(x, y).A > 0)
+                    if (colorValues[(y * TileSize.Width + x) * 4 + 3] > 0)
                     {
                         WidthMap[i][y]++;
                         HeightMap[i][x]++;
                     }
                 }
             }
+            Marshal.Copy(colorValues, 0, ptr, bytes);
+
+            Tiles[i].UnlockBits(bitmapData);
         }
     }
 
@@ -191,71 +205,49 @@ public class TileSet
     public void TileChangeLine(int tileIndex, Vector2<int> tilePosition, bool isLeftButtonPressed)
     {
         Bitmap tile = Tiles[tileIndex];
-        Color CurrentPixel = tile.GetPixel(tilePosition.X, tilePosition.Y);
-        Rectangle rectangle = new Rectangle(0, 0, TileSize.Width, TileSize.Height);
+
+        BitmapData bitmapData = tile.LockBits(
+            new Rectangle(0, 0, TileSize.Width, TileSize.Height),
+            ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+        IntPtr ptr = bitmapData.Scan0;
+        int bytes = Math.Abs(bitmapData.Stride) * TileSize.Height;
+        var colorValues = new byte[bytes];
+
+        Marshal.Copy(ptr, colorValues, 0, bytes);
 
         if (isLeftButtonPressed)
         {
-            BitmapData bitmapData = tile.LockBits(rectangle, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
-            IntPtr ptr = bitmapData.Scan0;
-            int bytes = Math.Abs(bitmapData.Stride) * TileSize.Height;
-            byte[] argbValues = new byte[bytes];
-            System.Runtime.InteropServices.Marshal.Copy(ptr, argbValues, 0, bytes);
-
-            for (int counter = 2; counter < argbValues.Length; counter += 3)
-                argbValues[counter] = 255;
-
-            System.Runtime.InteropServices.Marshal.Copy(argbValues, 0, ptr, bytes);
-            tile.UnlockBits(bitmapData);
-
-
-
-            if (CompareColors(CurrentPixel, Color.Transparent) || tilePosition.Y == 0 ||
-                !CompareColors(tile.GetPixel(tilePosition.X, tilePosition.Y - 1), Color.Transparent))
+            if (tilePosition.Y == 0 || colorValues[GetAlphaIndex(tilePosition.X, tilePosition.Y)] == 0 ||
+                colorValues[GetAlphaIndex(tilePosition.X, tilePosition.Y - 1)] != 0)
             {
                 for (int y = 0; y < TileSize.Height; y++)
                 {
-                    tile.MakeTransparent();
-                    tile.SetPixel(tilePosition.X, y, y < tilePosition.Y ? Color.Transparent : Color.Black);
-                    Color color = tile.GetPixel(tilePosition.X, y);
-                    if (CompareColors(Color.Transparent, color))
-                    {
-                        Color test = Color.FromArgb(0, 0, 0, 0);
-                        continue;
-                    }
+                    colorValues[GetAlphaIndex(tilePosition.X, y)] = (byte)(y < tilePosition.Y ? 0 : 255);
                 }
             }
             else
             {
                 for (int y = 0; y < TileSize.Height; y++)
                 {
-                    tile.SetPixel(tilePosition.X, y, Color.Transparent);
+                    colorValues[GetAlphaIndex(tilePosition.X, y)] = 0;
                 }
             }
         }
         else
         {
-            if (CurrentPixel == Color.Transparent || CurrentPixel != Color.Transparent
-                && (tilePosition.X == 0 || tile.GetPixel(tilePosition.X - 1, tilePosition.Y) != Color.Transparent))
-            {
-                for (int x = 0; x < TileSize.Height; x++)
-                {
-                    tile.SetPixel(x, tilePosition.Y, x < tilePosition.X ? Color.Transparent : Color.Black);
-                }
-            }
-            else
-            {
-                for (int x = 0; x < TileSize.Height; x++)
-                {
-                    tile.SetPixel(x, tilePosition.Y, Color.Transparent);
-                }
-            }
+            int alphaOnPositionIndex = GetAlphaIndex(tilePosition.X, tilePosition.Y);
+            colorValues[alphaOnPositionIndex] = (byte)(colorValues[alphaOnPositionIndex] == 0 ? 255 : 0);
         }
+
+        Marshal.Copy(colorValues, 0, ptr, bytes);
+
+        tile.UnlockBits(bitmapData);
     }
 
-    private bool CompareColors(Color first, Color second)
+    private int GetAlphaIndex(int positionX, int positionY)
     {
-        return first.ToArgb() == second.ToArgb();
+        return (positionY * TileSize.Width + positionX) * 4 + 3;
     }
 
     public void InsertTile(int tileIndex)
