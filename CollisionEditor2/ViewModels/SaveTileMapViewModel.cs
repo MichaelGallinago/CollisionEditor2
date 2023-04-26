c5714bca51c66b55b2c2ec7a13e45f2a72e58557
informant <artemiy.solodkin@mail.ru>
2023-04-26 22:48:51 +0500

    Lv of a Window

--- CollisionEditor2/ViewModels/SaveTileMapViewModel.cs
+++ CollisionEditor2/ViewModels/SaveTileMapViewModel.cs
@@ -25,45 +25,7 @@ namespace CollisionEditor2.ViewModels
 
         public ReactiveCommand<Unit, Unit> SaveCommand { get; }
         public ReactiveCommand<Unit, Unit> AddGroupCommand { get; }
-        public string TileHeightText
-        {
-            get => tileHeight.ToString();
-            set
-            {
-                textboxValidator.ClearErrors(nameof(TileHeightText));
-                CheckErrors();
-                bool isNumber = int.TryParse(value, out int intTileHeight);
-
-                if (!isNumber || intTileHeight < minTileHeight)
-                {
-                    textboxValidator.AddError(nameof(TileHeightText), "Wrong Tile Height!");
-                    CheckErrors();
-                    return;
-                }
-
-                tileHeight = intTileHeight;
-            }
-        }
-
-        public string TileWidthText
-        {
-            get => tileWidth.ToString();
-            set
-            {
-                textboxValidator.ClearErrors(nameof(TileWidthText));
-                CheckErrors();
-                bool isNumber = int.TryParse(value, out int intTileWidth);
-
-                if (!isNumber || intTileWidth < minTileWidth)
-                {
-                    textboxValidator.AddError(nameof(TileWidthText), "Wrong Tile Width!");
-                    CheckErrors();
-                    return;
-                }
-
-                tileWidth = intTileWidth;
-            }
-        }
+        
         public string VerticalSeparationText
         {
             get => verticalSeparation.ToString();
@@ -162,6 +124,104 @@ namespace CollisionEditor2.ViewModels
             }
         }
 
+        public string RedChannel1Text
+        {
+            get => redChannel1.ToString();
+            set
+            {
+                textboxValidator.ClearErrors(nameof(RedChannel1Text));
+                CheckErrors();
+                bool isNumber = int.TryParse(value, out int intRedChannel1);
+
+                if (!isNumber || intRedChannel1 < 0|| intRedChannel1>255)
+                {
+                    textboxValidator.AddError(nameof(RedChannel1Text), "Not a Byte!");
+                    CheckErrors();
+                    return;
+                }
+
+                redChannel1 = intRedChannel1;
+            }
+        }
+        public string GreenChannel1Text
+        {
+            get => greenChannel1.ToString();
+            set
+            {
+                textboxValidator.ClearErrors(nameof(GreenChannel1Text));
+                CheckErrors();
+                bool isNumber = int.TryParse(value, out int intGreenChannel1);
+
+                if (!isNumber || intGreenChannel1 < 0 || intGreenChannel1 > 255)
+                {
+                    textboxValidator.AddError(nameof(GreenChannel1Text), "Not a Byte!");
+                    CheckErrors();
+                    return;
+                }
+
+                greenChannel1 = intGreenChannel1;
+            }
+        }
+        public string BlueChannel1Text
+        {
+            get => blueChannel1.ToString();
+            set
+            {
+                textboxValidator.ClearErrors(nameof(BlueChannel1Text));
+                CheckErrors();
+                bool isNumber = int.TryParse(value, out int intBlueChannel1);
+
+                if (!isNumber || intBlueChannel1 < 0 || intBlueChannel1 > 255)
+                {
+                    textboxValidator.AddError(nameof(BlueChannel1Text), "Not a Byte!");
+                    CheckErrors();
+                    return;
+                }
+
+                blueChannel1 = intBlueChannel1;
+            }
+        }
+
+        public string AlphaChannel1Text
+        {
+            get => alphaChannel1.ToString();
+            set
+            {
+                textboxValidator.ClearErrors(nameof(AlphaChannel1Text));
+                CheckErrors();
+                bool isNumber = int.TryParse(value, out int intAlphaChannel1);
+
+                if (!isNumber || intAlphaChannel1 < 0 || intAlphaChannel1 > 255)
+                {
+                    textboxValidator.AddError(nameof(AlphaChannel1Text), "Not a Byte!");
+                    CheckErrors();
+                    return;
+                }
+
+                alphaChannel1 = intAlphaChannel1;
+            }
+        }
+
+        public string OffsetInTiles1Text
+        {
+            get => offsetInTiles1.ToString();
+            set
+            {
+                textboxValidator.ClearErrors(nameof(OffsetInTiles1Text));
+                CheckErrors();
+                bool isNumber = int.TryParse(value, out int intOffsetInTiles1);
+
+                if (!isNumber || intOffsetInTiles1 < 0 || intOffsetInTiles1 > 255)
+                {
+                    textboxValidator.AddError(nameof(OffsetInTiles1Text), "Wrong Offset!");
+                    CheckErrors();
+                    return;
+                }
+
+                offsetInTiles1 = intOffsetInTiles1;
+            }
+        }
+
         private void CheckErrors()
         {
             if (textboxValidator.HasErrors)
@@ -175,13 +235,17 @@ namespace CollisionEditor2.ViewModels
         }
 
 
-        private int tileHeight;
-        private int tileWidth;
         private int verticalSeparation;
         private int horizontalSeparation;
         private int verticalOffset;
         private int horizontalOffset;
         private int amountOfColumns;
+
+        private int redChannel1;
+        private int greenChannel1;
+        private int blueChannel1;
+        private int alphaChannel1;
+        private int offsetInTiles1;
 
         private readonly TextboxValidator textboxValidator;
         public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
@@ -194,8 +258,6 @@ namespace CollisionEditor2.ViewModels
             SaveCommand     = ReactiveCommand.Create(Save);
 
             this.window = window;
-            tileHeight = minTileHeight;
-            tileWidth = minTileWidth;
         }
 
         
@@ -222,7 +284,6 @@ namespace CollisionEditor2.ViewModels
         private void TextboxValidator_ErrorsChanged(object? sender, DataErrorsChangedEventArgs e)
         {
             ErrorsChanged?.Invoke(this, e);
-            OnPropertyChanged(nameof(tileHeight));
         }
 
         public IEnumerable GetErrors(string? propertyName)
