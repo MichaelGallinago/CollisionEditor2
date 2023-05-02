0e77445fd136bc9ecfa45caab33ae79e0b4507d2
informant <artemiy.solodkin@mail.ru>
2023-05-02 18:46:40 +0500

    Fixed

--- CollisionEditor2/ViewModels/MainViewModel.cs
+++ CollisionEditor2/ViewModels/MainViewModel.cs
@@ -383,7 +383,7 @@ public class MainViewModel : ViewModelBase, INotifyDataErrorInfo
     }
 
     private void MenuSaveAll()
-    {
+    {   
         MenuSaveAngleMap();
         MenuSaveHeightMap();
         MenuSaveWidthMap();
