10471626586ca86eae0aa2a050b36e4ef3f78d00
Michael Gallinago <michael.gallinago@gmail.com>
2023-05-07 22:06:00 +0500

    Hotfix

--- CollisionEditor2/Models/ForAvalonia/ColorGroup.cs
+++ CollisionEditor2/Models/ForAvalonia/ColorGroup.cs
@@ -2,21 +2,18 @@
 
 public class ColorGroup
 {
-    public struct ColorGroup
+    public byte RedChannel    { get; set;} 
+    public byte GreenChannel  { get; set; }
+    public byte BlueChannel   { get; set; }
+    public byte AlphaChannel  { get; set; }
+    public int  OffsetInTiles { get; set; }
+
+    public ColorGroup(byte redChannel, byte greenChannel, byte blueChannel, byte alphaChannel, int offsetInTiles) 
     {
-        
-        public byte RedChannel    { get; set;} 
-        public byte GreenChannel  { get; set; }
-        public byte BlueChannel   { get; set; }
-        public byte AlphaChannel  { get; set; }
-        public int  OffsetInTiles { get; set; }
-        public ColorGroup(byte redChannel, byte greenChannel, byte blueChannel, byte alphaChannel, int offsetInTiles) 
-        {
-            RedChannel = redChannel;
-            GreenChannel = greenChannel;
-            BlueChannel = blueChannel;
-            AlphaChannel = alphaChannel;
-            OffsetInTiles = offsetInTiles;
-        }
+        RedChannel = redChannel;
+        GreenChannel = greenChannel;
+        BlueChannel = blueChannel;
+        AlphaChannel = alphaChannel;
+        OffsetInTiles = offsetInTiles;
     }
 }
