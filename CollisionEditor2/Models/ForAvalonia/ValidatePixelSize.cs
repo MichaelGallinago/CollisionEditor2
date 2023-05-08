c260e4fb22ba261174fe329a0bfd326150c8f026
informant <artemiy.solodkin@mail.ru>
2023-05-08 14:43:28 +0500

    ENOUGH

--- CollisionEditor2/Models/ForAvalonia/ValidatePixelSize.cs
+++ CollisionEditor2/Models/ForAvalonia/ValidatePixelSize.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CollisionEditor2.Models.ForAvalonia
+﻿namespace CollisionEditor2.Models.ForAvalonia
 {
-    public class ValidatePixelSize
+    public class ValidatedPixelSize
     {
         public int     Width        { get; set;}
         public int     Height       { get; set;}
         public string WidthString  { get; set;}
         public string HeightString { get; set;}
 
-        public ValidatePixelSize(int width, int height)
+        public ValidatedPixelSize(int width, int height)
         {
             WidthString = width.ToString();
             HeightString = height.ToString();
